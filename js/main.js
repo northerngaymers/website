33d9e9a067403a1c9b1be7a7b76993de7226debc
northerngaymers <>
2019-09-10 10:48:32 +0100

    update

--- js/main.js
+++ js/main.js
@@ -34,58 +34,6 @@ preloader
 });
     
     
-/*---------------------
-  Feedback Slider
---------------------- */
- var slider = document.getElementById("myRange1");
-
-
-  slider.addEventListener('mouseup', function(event) {
-    var x = event.target.value
-    console.log(x)
-    if (x <= 5) {
-        slider.className = '';
-        slider.className = `MyClass-${x}`;
-        slider.className = `MyClass-${x}`;
-        slider.className = `MyClass-${x}`;
-        slider.className = `MyClass-${x}`;
-        slider.className = `MyClass-${x}`;
-    }
-  }) 
-    
-    var slider2 = document.getElementById("myRange2");
-
-
-  slider2.addEventListener('mouseup', function(event) {
-    var x = event.target.value
-    console.log(x)
-    if (x <= 5) {
-        slider2.className = '';
-        slider2.className = `MyClass-${x}`;
-        slider2.className = `MyClass-${x}`;
-        slider2.className = `MyClass-${x}`;
-        slider2.className = `MyClass-${x}`;
-        slider2.className = `MyClass-${x}`;
-    }
-  }) 
-    
-    var slider3 = document.getElementById("myRange3");
-
-
-  slider3.addEventListener('mouseup', function(event) {
-    var x = event.target.value
-    console.log(x)
-    if (x <= 5) {
-        slider3.className = '';
-        slider3.className = `MyClass-${x}`;
-        slider3.className = `MyClass-${x}`;
-        slider3.className = `MyClass-${x}`;
-        slider3.className = `MyClass-${x}`;
-        slider3.className = `MyClass-${x}`;
-    }
-  }) 
-    
-    
     
 
 /*---------------------
--- js/slider.js
+++ js/slider.js
@@ -1,34 +1,47 @@
-const slider = new Vue ({
-	el: '#slider',
-	data(){
-		return {
-			val: 70
-		}
-	},
-	mounted(){
-		this.val = Math.floor(Math.random() * 101)
-	},
-	computed: {
-		getPlacement: function(){
-			return `${(-0.009 * ((this.val * -1) + 104))}em`;
-		},
-		greaterThanFifty: function(){
-			return this.val > 50 ? `var(--roundness)` : `0`;
-		},
-		getHappinessColor: function(){
-  		return `rgba(255, ${106 + (103 / 100 * this.val)}, ${(Math.floor(this.val * -1 / 7.692)) + 13}`;
-		},
-		getSliderBackground: function(){
-			return `linear-gradient(to right, var(--orange), ${(this.val * -1) + 125}%, var(--yellow))`
-		},
-		getHappiness: function(){
-			//This used to be a lengthy if/else; shoutout to @raddevus for suggesting this clever method to shorten the code
-			let moods = ["😡","😠","😦","☹️","🙁","😐","🙂","😊","😄","😃","😍"]
-			if(this.val == 0){
-				return "🤬";
-			} else {
-				return moods[(Math.floor(this.val / 10))];
-			}
-		}
-	}
-});
+var slider = document.getElementById("myRange1");
+
+
+  slider.addEventListener('mouseup', function(event) {
+    var x = event.target.value
+    console.log(x)
+    if (x <= 5) {
+        slider.className = '';
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+    }
+  }) 
+    
+    var slider2 = document.getElementById("myRange2");
+
+
+  slider2.addEventListener('mouseup', function(event) {
+    var x = event.target.value
+    console.log(x)
+    if (x <= 5) {
+        slider2.className = '';
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+    }
+  }) 
+    
+    var slider3 = document.getElementById("myRange3");
+
+
+  slider3.addEventListener('mouseup', function(event) {
+    var x = event.target.value
+    console.log(x)
+    if (x <= 5) {
+        slider3.className = '';
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+    }
+  }) 
