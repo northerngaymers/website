ef8743b7b61b559ef3bfdfac7f605146ed90c5c2
northerngaymers <>
2019-09-10 02:44:37 +0100

    update

--- js/main.js
+++ js/main.js
@@ -32,6 +32,61 @@ preloader
 }, function() {
     $(this).animate({opacity: 0.9}, 300);
 });
+    
+    
+/*---------------------
+  Feedback Slider
+--------------------- */
+ var slider = document.getElementById("myRange1");
+
+
+  slider.addEventListener('mouseup', function(event) {
+    var x = event.target.value
+    console.log(x)
+    if (x <= 5) {
+        slider.className = '';
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+        slider.className = `MyClass-${x}`;
+    }
+  }) 
+    
+    var slider2 = document.getElementById("myRange2");
+
+
+  slider2.addEventListener('mouseup', function(event) {
+    var x = event.target.value
+    console.log(x)
+    if (x <= 5) {
+        slider2.className = '';
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+        slider2.className = `MyClass-${x}`;
+    }
+  }) 
+    
+    var slider3 = document.getElementById("myRange3");
+
+
+  slider3.addEventListener('mouseup', function(event) {
+    var x = event.target.value
+    console.log(x)
+    if (x <= 5) {
+        slider3.className = '';
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+        slider3.className = `MyClass-${x}`;
+    }
+  }) 
+    
+    
+    
 
 /*---------------------
   venobox
